d1498d5bf9848b74554e68c6b43085b825924821
Pogos Torosian <pogosstudio@gmail.com>
2025-09-22 17:10:00 +0400

    feat: solve formattime task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -264,8 +264,16 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  let min = minutes;
+  let sec = seconds;
+  if (seconds < 10) {
+    sec = seconds.toString().padStart(2, '0');
+  }
+  if (minutes < 10) {
+    min = minutes.toString().padStart(2, '0');
+  }
+  return `${min}:${sec}`;
 }
 
 /**
