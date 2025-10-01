013c3d9ddc8bfd97ba67fdccd1a8c44e28f0fb7b
Pogos Torosian <pogosstudio@gmail.com>
2025-10-01 11:38:48 +0400

    feat: solve countVowels task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -338,8 +338,19 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  let counter = 0;
+  for (let i = 0; i < str.length; ) {
+    for (let j = 0; j < vowels.length; ) {
+      if (str[i] === vowels[j]) {
+        counter += 1;
+      }
+      j += 1;
+    }
+    i += 1;
+  }
+  return counter;
 }
 
 /**
